0daaa048e06a9e28bb9ae5fad04d8142ff34bd9b
wugenshui <963838470@qq.com>
2020-02-26 14:46:59 +0800

    视频播放

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import protooClient from "protoo-client"
-import { Text, TouchableOpacity, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { button, container, rtcView, text } from "./styles"
 import { log, logError } from "./debug"
 
@@ -23,8 +23,11 @@ import { getProtooUrl } from "./lib/urlFactory"
 class App extends Component {
   constructor(props) {
     super(props)
-    console.debug("\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n")
-    console.debug("----------------APP初始化--------------")
+    console.info("")
+    console.info("")
+    console.info("")
+    console.info("")
+    console.info("----------------APP初始化--------------")
     global.RTCPeerConnection = RTCPeerConnection
     global.RTCIceCandidate = RTCIceCandidate
     global.RTCSessionDescription = RTCSessionDescription
@@ -32,9 +35,10 @@ class App extends Component {
     global.MediaStreamTrack = MediaStreamTrack
     global.RTCView = RTCView
     global.navigator = { mediaDevices: { getUserMedia }, product: "ReactNative" }
+    global.myapp = this
     const peerName = "mypeername" + Math.random()
     const roomId = "myroomid"
-    const displayName = "chenbo"
+    const displayName = "Webrtc Demo"
     const useSimulcast = true
     const forceTcp = false
     const spy = false
@@ -78,13 +82,13 @@ class App extends Component {
 
     console.log("在初始化RoomClient之前", roomId, peerName, displayName, device, useSimulcast, forceTcp, spy, forceH264)
     var roomClient = new RoomClient({ roomId, peerName, displayName, device, useSimulcast, forceTcp, spy, forceH264 })
-    console.log("在初始化RoomClient之后", roomClient)
+    //console.log("在初始化RoomClient之后", roomClient)
 
     // NOTE: For debugging.
     global.CLIENT = roomClient
   }
   state = {
-    info: "Initializing",
+    info: "初始化",
     status: "init",
     roomID: "abc",
     isFront: true,
@@ -95,10 +99,19 @@ class App extends Component {
     const { status, info, streamURL, remoteList } = this.state
     return (
       <View>
-        <Text>hello world</Text>
+        <Text>{this.state.info}</Text>
+        <RTCView style={styles.box} streamURL={this.state.videoURL} />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: '100%',
+    height: 400,
+    backgroundColor: "lightgray"
+  }
+})
 
 export default App
